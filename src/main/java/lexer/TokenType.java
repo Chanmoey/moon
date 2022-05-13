36e00f62a92b1e476f33298ba5bc982852535fee
Chanmoey <relijifa@gmail.com>
2022-05-13 10:56:32 +0800

    定义关键字类型。

---
+++ src/main/java/lexer/TokenType.java
@@ -0,0 +1,16 @@
+package lexer;
+
+/**
+ * @author Chanmoey
+ * @date 2022年05月11日
+ */
+public enum TokenType {
+    KEYWORD,
+    VARIABLE,
+    OPERATOR,
+    BRACKET,
+    INTEGER,
+    STRING,
+    FLOAT,
+    BOOLEAN
+}
