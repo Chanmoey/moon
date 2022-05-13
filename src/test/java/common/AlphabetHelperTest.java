0003bea52ca6d026b31ce7dd8c2709dcb5b1fc15
Chanmoey <relijifa@gmail.com>
2022-05-13 15:38:12 +0800

    单元测试类。

---
+++ src/test/java/common/AlphabetHelperTest.java
@@ -0,0 +1,38 @@
+package common;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Chanmoey
+ * @date 2022年05月13日
+ */
+public class AlphabetHelperTest {
+
+    @Test
+    public void test() {
+        // letter
+        assertTrue(AlphabetHelper.isLetter('a'));
+        assertFalse(AlphabetHelper.isLetter('*'));
+        assertFalse(AlphabetHelper.isLetter('*'));
+
+        // literal
+        assertTrue(AlphabetHelper.isLiteral('a'));
+        assertTrue(AlphabetHelper.isLiteral('_'));
+        assertTrue(AlphabetHelper.isLiteral('9'));
+        assertFalse(AlphabetHelper.isLiteral('*'));
+
+        // number
+        assertTrue(AlphabetHelper.isNumber('6'));
+        assertTrue(AlphabetHelper.isNumber('0'));
+        assertFalse(AlphabetHelper.isNumber('x'));
+
+        //operator
+        assertTrue(AlphabetHelper.isOperator('+'));
+        assertTrue(AlphabetHelper.isOperator('*'));
+        assertTrue(AlphabetHelper.isOperator('/'));
+        assertTrue(AlphabetHelper.isOperator('&'));
+        assertFalse(AlphabetHelper.isOperator('a'));
+    }
+}
