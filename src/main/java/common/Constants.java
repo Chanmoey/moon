2b28b20cc9b3203e2b224372d2dde13df71d1396
Chanmoey <relijifa@gmail.com>
2022-05-13 15:38:27 +0800

    定义一些常量。

---
+++ src/main/java/common/Constants.java
@@ -0,0 +1,10 @@
+package common;
+
+/**
+ * @author Chanmoey
+ * @date 2022年05月13日
+ */
+public class Constants {
+    public static final String TRUE = "true";
+    public static final String FALSE = "false";
+}
