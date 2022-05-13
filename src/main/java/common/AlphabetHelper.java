85790a5b6c1f5d0aada8d09f3cba03a1a73a12ad
Chanmoey <relijifa@gmail.com>
2022-05-13 15:30:49 +0800

    判断字符的正则助手类。

---
+++ src/main/java/common/AlphabetHelper.java
@@ -0,0 +1,34 @@
+package common;
+
+import java.util.regex.Pattern;
+
+/**
+ * @author Chanmoey
+ * @date 2022年05月13日
+ */
+public class AlphabetHelper {
+
+    private static final Pattern LETTER = Pattern.compile("^[a-zA-z]$");
+
+    private static final Pattern NUMBER = Pattern.compile("^\\d$");
+
+    private static final Pattern LITERAL = Pattern.compile("^[_a-zA-z\\d]$");
+
+    private static final Pattern OPERATOR = Pattern.compile("^[+-\\\\*<>=!&|^%/]$");
+
+    public static boolean isLetter(char c) {
+        return LETTER.matcher(String.valueOf(c)).matches();
+    }
+
+    public static boolean isNumber(char c) {
+        return NUMBER.matcher(String.valueOf(c)).matches();
+    }
+
+    public static boolean isLiteral(char c) {
+        return LITERAL.matcher(String.valueOf(c)).matches();
+    }
+
+    public static boolean isOperator(char c) {
+        return OPERATOR.matcher(String.valueOf(c)).matches();
+    }
+}
