25a799859d498d88c08b55a5be7c77a248e683fe
Chanmoey <relijifa@gmail.com>
2022-05-13 15:39:30 +0800

    Token类。

--- src/main/java/lexer/Token.java
+++ src/main/java/lexer/Token.java
@@ -1,13 +1,17 @@
 package lexer;
 
+import common.AlphabetHelper;
+import common.Constants;
+import common.PeekIterator;
+
 /**
  * @author Chanmoey
  * @date 2022年05月11日
  */
 public class Token {
 
-    private TokenType type;
-    private String value;
+    private final TokenType type;
+    private final String value;
 
     public Token(TokenType type, String value) {
         this.type = type;
@@ -27,6 +31,31 @@ public class Token {
                 || this.type == TokenType.FLOAT
                 || this.type == TokenType.STRING
                 || this.type == TokenType.BOOLEAN;
+    }
+
+    public static Token makeVarOrKeyword(PeekIterator<Character> iterator) {
+        StringBuilder sb = new StringBuilder();
+
+        while (iterator.hasNext()) {
+            Character ahead = iterator.peek();
+            if (AlphabetHelper.isLiteral(ahead)) {
+                sb.append(ahead);
+            } else {
+                break;
+            }
+        }
+        iterator.next();
+
+        String s = sb.toString();
+        if (KeyWords.isKeyword(s)) {
+            return new Token(TokenType.KEYWORD, s);
+        }
+
+        if (Constants.TRUE.equals(s) || Constants.FALSE.equals(s)) {
+            return new Token(TokenType.BOOLEAN, s);
+        }
+
+        return new Token(TokenType.VARIABLE, s);
     }
 
     @Override
