ca1f571b8f527da7c4766a5220c3e1238eee51fc
Chanmoey <relijifa@gmail.com>
2022-05-23 20:57:20 +0800

    词法分析器。

--- src/main/java/lexer/Lexer.java
+++ src/main/java/lexer/Lexer.java
@@ -1,6 +1,10 @@
 package lexer;
 
+import common.AlphabetHelper;
+import common.PeekIterator;
+
 import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 /**
@@ -9,7 +13,66 @@ import java.util.stream.Stream;
  */
 public class Lexer {
 
-    public ArrayList<Token> analyse(Stream<?> source) {
-        return null;
+    public List<Token> analyse(Stream<?> source) throws LexicalException {
+        List<Token> tokenList = new ArrayList<>();
+        PeekIterator<Character> iterator = new PeekIterator<>((Stream<Character>) source, (char) 0);
+
+        while (iterator.hasNext()) {
+            char c = iterator.next();
+
+            if (c == 0) {
+                break;
+            }
+
+            char lookahead = iterator.peek();
+
+            if (c == ' ' || c == '\n') {
+                continue;
+            }
+
+            if (c == '{' || c == '}' || c == '(' || c == ')') {
+                tokenList.add(new Token(TokenType.BRACKET, String.valueOf(c)));
+                continue;
+            }
+
+            if (c == '"' || c == '\'') {
+                iterator.pushBack();
+                tokenList.add(Token.makeString(iterator));
+                continue;
+            }
+
+            if (AlphabetHelper.isLetter(c)) {
+                iterator.pushBack();
+                tokenList.add(Token.makeVarOrKeyword(iterator));
+                continue;
+            }
+
+            if (AlphabetHelper.isNumber(c)) {
+                iterator.pushBack();
+                tokenList.add(Token.makeNumber(iterator));
+                continue;
+            }
+
+            // + -: 3 + 5, +5, 3 * -5
+            if ((c == '+' || c == '-' || c == '.') && AlphabetHelper.isNumber(lookahead)) {
+                Token lastToken = tokenList.isEmpty() ? null : tokenList.get(tokenList.size() - 1);
+
+                if (lastToken == null || lastToken.isNumber() || lastToken.isOperator()) {
+                    iterator.pushBack();
+                    tokenList.add(Token.makeNumber(iterator));
+                    continue;
+                }
+            }
+
+            if (AlphabetHelper.isOperator(c)) {
+                iterator.pushBack();
+                tokenList.add(Token.makeOp(iterator));
+                continue;
+            }
+
+            throw new LexicalException(c);
+        } // end while
+
+        return tokenList;
     }
 }
