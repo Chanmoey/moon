b75d8036bed8390746ab7c778ef4bdf651396ceb
Chanmoey <relijifa@gmail.com>
2022-05-25 19:18:56 +0800

    定义一些常量。

--- src/main/java/common/Constants.java
+++ src/main/java/common/Constants.java
@@ -11,4 +11,5 @@ public class Constants {
     public static final String TRUE = "true";
     public static final String FALSE = "false";
     public static final String UNEXPECTED_ERROR = "Unexpected error";
+    public static final String COMMENTS_NOT_MATCH = "comments not match";
 }
