40e9809936f25fc31b6a2551eb2946262ac8acd6
Chanmoey <relijifa@gmail.com>
2022-05-27 09:05:42 +0800

    函数声明语句。

---
+++ src/main/java/parser/DefStmt.java
@@ -0,0 +1,11 @@
+package parser;
+
+/**
+ * @author Chanmoey
+ * @date 2022年05月27日
+ */
+public class DefStmt extends Stmt{
+    protected DefStmt(ASTNode parent) {
+        super(parent, ASTNodeTypes.DEF_STMT, "def");
+    }
+}
