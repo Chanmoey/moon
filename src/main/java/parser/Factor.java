c21ab3911c8db8acc117d01e325967952142a710
Chanmoey <relijifa@gmail.com>
2022-05-27 09:06:27 +0800

    因子，表达式的操作数。

---
+++ src/main/java/parser/Factor.java
@@ -0,0 +1,11 @@
+package parser;
+
+/**
+ * @author Chanmoey
+ * @date 2022年05月27日
+ */
+public abstract class Factor extends ASTNode{
+    protected Factor(ASTNode parent, ASTNodeTypes type, String label) {
+        super(parent, type, label);
+    }
+}
