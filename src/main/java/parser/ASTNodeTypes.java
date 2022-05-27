1ba8d3c51976059a695e1a884ee500e4263b0592
Chanmoey <relijifa@gmail.com>
2022-05-27 09:04:55 +0800

    语法树节点的类型。

---
+++ src/main/java/parser/ASTNodeTypes.java
@@ -0,0 +1,19 @@
+package parser;
+
+/**
+ * @author Chanmoey
+ * @date 2022年05月27日
+ */
+public enum ASTNodeTypes {
+    BLOCK,
+    BINARY_EXPR,
+    UNARY_EXPR,
+    VARIABLE,
+    SCALAR,
+    IF_STMT,
+    WHILE_STMT,
+    FOR_STMT,
+    ASSIGN_STMT,
+    DEF_STMT,
+    DECLARE_STMT
+}
