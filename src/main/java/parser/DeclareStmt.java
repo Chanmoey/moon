32340d39353af7c0c165268226356d3e918dcb70
Chanmoey <relijifa@gmail.com>
2022-05-27 09:05:28 +0800

    声明语句。

---
+++ src/main/java/parser/DeclareStmt.java
@@ -0,0 +1,11 @@
+package parser;
+
+/**
+ * @author Chanmoey
+ * @date 2022年05月27日
+ */
+public class DeclareStmt extends Stmt{
+    public DeclareStmt(ASTNode parent) {
+        super(parent, ASTNodeTypes.DECLARE_STMT, "declare");
+    }
+}
