663c5b0d99377ea8c7a12812caf61f42723522e7
Chanmoey <relijifa@gmail.com>
2022-05-27 10:41:05 +0800

    Token流。

---
+++ src/main/java/parser/utils/PeekTokenIterator.java
@@ -0,0 +1,35 @@
+package parser.utils;
+
+import common.PeekIterator;
+import lexer.Token;
+import lexer.TokenType;
+
+import java.util.stream.Stream;
+
+/**
+ * @author Chanmoey
+ * @date 2022年05月27日
+ */
+public class PeekTokenIterator extends PeekIterator<Token> {
+    public PeekTokenIterator(Stream<Token> stream) {
+        super(stream);
+    }
+
+    public Token nextMatch(String value) throws ParseException {
+        Token token = this.next();
+        if (!token.getValue().equals(value)) {
+            throw new ParseException(token);
+        }
+
+        return token;
+    }
+
+    public Token nextMatch(TokenType type) throws ParseException {
+        Token token = this.next();
+        if (!token.getType().equals(type)) {
+            throw new ParseException(token);
+        }
+
+        return token;
+    }
+}
