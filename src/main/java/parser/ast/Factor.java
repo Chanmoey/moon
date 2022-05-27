102bea57d197a35d7b4c43b107102d2935594aa6
Chanmoey <relijifa@gmail.com>
2022-05-27 16:40:47 +0800

    语法分析树的因子节点，操作数。

--- src/main/java/parser/ast/Factor.java
+++ src/main/java/parser/ast/Factor.java
@@ -1,11 +1,27 @@
 package parser.ast;
 
+import lexer.Token;
+import lexer.TokenType;
+import parser.utils.PeekTokenIterator;
+
 /**
  * @author Chanmoey
  * @date 2022年05月27日
  */
 public abstract class Factor extends ASTNode{
-    protected Factor(ASTNode parent, ASTNodeTypes type, String label) {
-        super(parent, type, label);
+    protected Factor(ASTNode parent, PeekTokenIterator iterator) {
+        super(parent);
+
+        Token token = iterator.next();
+        TokenType tokenType = token.getType();
+
+        if (tokenType == TokenType.VARIABLE) {
+            this.type = ASTNodeTypes.VARIABLE;
+        } else {
+            this.type = ASTNodeTypes.SCALAR;
+        }
+
+        this.label = token.getValue();
+        this.lexeme = token;
     }
 }
