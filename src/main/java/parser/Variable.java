469fdf19a12b27185e66029ad8a7489f7ca2918d
Chanmoey <relijifa@gmail.com>
2022-05-27 09:07:33 +0800

    变量。

---
+++ src/main/java/parser/Variable.java
@@ -0,0 +1,11 @@
+package parser;
+
+/**
+ * @author Chanmoey
+ * @date 2022年05月27日
+ */
+public class Variable extends Factor{
+    protected Variable(ASTNode parent) {
+        super(parent, ASTNodeTypes.VARIABLE, "variable");
+    }
+}
