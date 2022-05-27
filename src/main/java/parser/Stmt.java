d76cc47a01c2dc8d05bcb572a8917816e9766449
Chanmoey <relijifa@gmail.com>
2022-05-27 09:07:23 +0800

    语句抽象类。

---
+++ src/main/java/parser/Stmt.java
@@ -0,0 +1,12 @@
+package parser;
+
+/**
+ * @author Chanmoey
+ * @date 2022年05月27日
+ */
+public abstract class Stmt extends ASTNode{
+
+    protected Stmt(ASTNode parent, ASTNodeTypes type, String label) {
+        super(parent, type, label);
+    }
+}
