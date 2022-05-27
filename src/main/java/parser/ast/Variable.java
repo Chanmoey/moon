7dfa68243e8e477dcccd6b992fb29b6bf4b46410
Chanmoey <relijifa@gmail.com>
2022-05-27 16:42:17 +0800

    变量类型的语法树节点。

--- src/main/java/parser/ast/Variable.java
+++ src/main/java/parser/ast/Variable.java
@@ -1,11 +1,13 @@
 package parser.ast;
 
+import parser.utils.PeekTokenIterator;
+
 /**
  * @author Chanmoey
  * @date 2022年05月27日
  */
 public class Variable extends Factor{
-    protected Variable(ASTNode parent) {
-        super(parent, ASTNodeTypes.VARIABLE, "variable");
+    protected Variable(ASTNode parent, PeekTokenIterator iterator) {
+        super(parent, iterator);
     }
 }
