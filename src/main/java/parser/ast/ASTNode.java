f0a46257787157594383cb9ffc79cac9f8469d95
Chanmoey <relijifa@gmail.com>
2022-05-27 16:39:14 +0800

    语法分析树的节点。

--- src/main/java/parser/ast/ASTNode.java
+++ src/main/java/parser/ast/ASTNode.java
@@ -1,14 +1,20 @@
 package parser.ast;
 
 import lexer.Token;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 /**
  * @author Chanmoey
  * @date 2022年05月26日
  */
+@Getter
+@Setter
 public abstract class ASTNode {
 
     /**
@@ -43,12 +49,36 @@ public abstract class ASTNode {
         this.children.add(node);
     }
 
-    public Token getLexeme() {
-        return lexeme;
-    }
+    public String print(ASTNode astNode) {
+        if (astNode == null) {
+            return "Empty Tree!";
+        }
+        List<List<String>> treeList = new ArrayList<>();
+        Deque<Object[]> queue = new ArrayDeque<>();
+        queue.add(new Object[]{astNode, 0});
+        while (!queue.isEmpty()) {
+            Object[] cur = queue.removeFirst();
+            int level = (Integer) cur[1];
+            ASTNode node = (ASTNode) cur[0];
+            if (level == treeList.size()) {
+                treeList.add(new ArrayList<>());
+            }
+            treeList.get(level).add(node.label);
 
-    public List<ASTNode> getChildren() {
-        return children;
+            for (ASTNode child : node.children) {
+                queue.add(new Object[]{child, level + 1});
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (List<String> strings : treeList) {
+            for (String string : strings) {
+                sb.append(string).append("    ");
+            }
+            sb.append('\n');
+        }
+
+        return sb.toString();
     }
 }
 
