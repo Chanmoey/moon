3e5271b4950d4d274d49b0ba5b728a16c24c3579
Chanmoey <relijifa@gmail.com>
2022-05-29 11:13:07 +0800

    操作符优先级表。

---
+++ src/main/java/parser/utils/PriorityTable.java
@@ -0,0 +1,30 @@
+package parser.utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Chanmoey
+ * @date 2022年05月29日
+ */
+public class PriorityTable {
+
+    private List<List<String>> table = new ArrayList<>();
+
+    public PriorityTable() {
+        table.add(Arrays.asList("&", "|", "^"));
+        table.add(Arrays.asList("==", "!=", ">", "<", ">=", "<="));
+        table.add(Arrays.asList("+", "-"));
+        table.add(Arrays.asList("*", "/"));
+        table.add(Arrays.asList("<<", ">>"));
+    }
+
+    public int size() {
+        return table.size();
+    }
+
+    public List<String> get(int level) {
+        return table.get(level);
+    }
+}
