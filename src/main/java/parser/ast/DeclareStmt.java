6c6edbafa1c375165aaf22ed006614e6d9f2902a
Chanmoey <relijifa@gmail.com>
2022-06-16 12:05:01 +0800

    添加程序解释方法。

--- src/main/java/parser/ast/DeclareStmt.java
+++ src/main/java/parser/ast/DeclareStmt.java
@@ -1,11 +1,31 @@
 package parser.ast;
 
+import parser.utils.ParseException;
+import parser.utils.PeekTokenIterator;
+
 /**
  * @author Chanmoey
  * @date 2022年05月27日
  */
-public class DeclareStmt extends Stmt{
+public class DeclareStmt extends Stmt {
     public DeclareStmt(ASTNode parent) {
         super(parent, ASTNodeTypes.DECLARE_STMT, "declare");
+    }
+
+    public static ASTNode parse(ASTNode parent, PeekTokenIterator iterator) throws ParseException {
+        var stmt = new DeclareStmt(parent);
+        iterator.nextMatch("moon");
+        var tkn = iterator.peek();
+        var factor = Factor.parse(parent, iterator);
+        if (factor == null) {
+            throw new ParseException(tkn);
+        }
+
+        stmt.addChild(factor);
+        var lexeme = iterator.nextMatch("=");
+        var expr = Expr.parse(iterator);
+        stmt.addChild(expr);
+        stmt.setLexeme(lexeme);
+        return stmt;
     }
 }
