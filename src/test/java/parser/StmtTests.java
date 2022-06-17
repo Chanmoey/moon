357480b4b1e2532c4a45b58eaa73440952bbc148
Chanmoey <relijifa@gmail.com>
2022-06-17 11:09:26 +0800

    单元测试。

--- src/test/java/parser/StmtTests.java
+++ src/test/java/parser/StmtTests.java
@@ -3,7 +3,7 @@ package parser;
 import lexer.Lexer;
 import lexer.LexicalException;
 import org.junit.jupiter.api.Test;
-import parser.ast.DeclareStmt;
+import parser.ast.*;
 import parser.utils.ParseException;
 import parser.utils.ParserUtils;
 import parser.utils.PeekTokenIterator;
@@ -22,6 +22,45 @@ class StmtTests {
         var stmt = DeclareStmt.parse(null, it);
         assertEquals(ParserUtils.toPostfixExpression(stmt), "i 100 2 * =");
         System.out.println(stmt.print(stmt));
+    }
+
+    @Test
+    void ifStmt() throws LexicalException, ParseException {
+        var it = createTokenIt("if(a){" +
+                "a = 1" +
+                "}"
+        );
+
+        var stmt = (IfStmt) IfStmt.parse(null, it);
+        var expr = (Variable) stmt.getChild(0);
+        var block = (Block) stmt.getChild(1);
+        var assignStmt = (AssignStmt) block.getChild(0);
+
+        assertEquals("a", expr.getLexeme().getValue());
+        assertEquals("=", assignStmt.getLexeme().getValue());
+    }
+
+    @Test
+    void ifElseStmt() throws LexicalException, ParseException {
+        var it = createTokenIt("if(a){" +
+                "a = 1" +
+                "} else {" +
+                "a = 2" +
+                "a = a * 3" +
+                "}"
+        );
+
+        var stmt = (IfStmt) IfStmt.parse(null, it);
+        var expr = (Variable) stmt.getChild(0);
+        var block = (Block) stmt.getChild(1);
+        var assignStmt = (AssignStmt) block.getChild(0);
+        var elseBlock = (Block) stmt.getChild(2);
+        var assignStmt2 = (AssignStmt) elseBlock.getChild(0);
+
+        assertEquals("a", expr.getLexeme().getValue());
+        assertEquals("=", assignStmt.getLexeme().getValue());
+        assertEquals("=", assignStmt2.getLexeme().getValue());
+        assertEquals(2, elseBlock.getChildren().size());
     }
 
     private PeekTokenIterator createTokenIt(String src) throws LexicalException {
