9bb167fd82ebb725fdade3b9f13b9ff4f99e69a3
Chanmoey <relijifa@gmail.com>
2022-06-17 11:06:33 +0800

    解析代码块。

--- src/main/java/parser/ast/Block.java
+++ src/main/java/parser/ast/Block.java
@@ -1,5 +1,8 @@
 package parser.ast;
 
+import parser.utils.ParseException;
+import parser.utils.PeekTokenIterator;
+
 /**
  * @author Chanmoey
  * @date 2022年05月27日
@@ -8,5 +11,17 @@ public class Block extends Stmt{
 
     public Block(ASTNode parent) {
         super(parent, ASTNodeTypes.BLOCK, "block");
+    }
+
+    public static  ASTNode parse(ASTNode parent, PeekTokenIterator iterator) throws ParseException {
+        iterator.nextMatch("{");
+        var block = new Block(parent);
+        ASTNode stmt;
+        while ((stmt = Stmt.parseStmt(parent, iterator)) != null) {
+            block.addChild(stmt);
+        }
+        iterator.nextMatch("}");
+
+        return block;
     }
 }
